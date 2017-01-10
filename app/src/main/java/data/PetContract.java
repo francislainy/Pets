dfc1af88b9f97defe64c2c5e4db362906fb6a7cb
Francislainy <francislainy.campos@gmail.com>
2017-01-10 15:07:35 +0000

    Initial commit plus Class Contract created.

---
+++ app/src/main/java/data/PetContract.java
@@ -0,0 +1,36 @@
+package data;
+
+import android.provider.BaseColumns;
+
+/**
+ * Created by Francislainy on 10/01/2017.
+ */
+
+public final class PetContract {
+    // To prevent someone from accidentally instantiating the contract class,
+    // Make the constructor private
+    private PetContract() {
+
+    }
+
+    /* Inner class that defines the table contents */
+    public static class PetEntry implements BaseColumns {
+
+        public static final String TABLE_NAME = "pets";
+
+        public static final String _ID = BaseColumns._ID;
+        public static final String COLUMN_PET_NAME = "name";
+        public static final String COLUMN_PET_BREED = "breed";
+        public static final String COLUMN_PET_GENDER = "gender";
+        public static final String COLUMN_PET_WEIGHT = "weight";
+
+        /**
+         * Possible values for the pet gender.
+         */
+        public static final int GENDER_UNKNOWN = 0;
+        public static final int GENDER_MALE = 1;
+        public static final int GENDER_FEMALE = 2;
+    }
+
+
+}
